e06fb91a4d0721848442cf238bb3a98f4af9afc8
Wawaann <bigotte.ewan@gmail.com>
2025-02-19 15:33:59 +0100

    init branch

--- src/main.c
+++ src/main.c
@@ -6,14 +6,27 @@
 /*   By: ebigotte <ebigotte@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 14:47:56 by ebigotte          #+#    #+#             */
-/*   Updated: 2024/12/27 14:52:18 by ebigotte         ###   ########.fr       */
+/*   Updated: 2025/02/19 15:20:58 by ebigotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft.h"
 
-int	main(void)
+int	main(int ac, char **av, char **env)
 {
-	ft_printf("Hello World !\n");
+	char *line;
+
+	while (1)
+	{
+		line = readline("$> ");
+		if (!line)
+			return (0);
+		if (ft_strncmp(line, "exit", 4) == 0)
+			break;
+		printf("line: '%s'\n", line);
+		add_history(line);
+		free(line);
+	}
+	clear_history();
 	return (0);
 }
