43d39c603be9bf045ee755b6b0e61bda2264ba66
Wawaann <bigotte.ewan@gmail.com>
2025-02-19 15:40:37 +0100

    start struct

--- include/ft.h
+++ include/ft.h
@@ -6,7 +6,7 @@
 /*   By: ebigotte <ebigotte@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 14:46:06 by ebigotte          #+#    #+#             */
-/*   Updated: 2025/02/19 14:06:18 by ebigotte         ###   ########.fr       */
+/*   Updated: 2025/02/19 15:34:25 by ebigotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,15 @@
 
 #include <readline/readline.h>
 #include <readline/history.h>
+
+typedef struct s_shell
+{
+    char    *input;       // Ligne de commande brute (récupérée avec readline)
+    char    **tokens;     // Commande découpée en tokens
+    char    ***cmds;      // Tableau des commandes séparées par `|`
+    int     num_cmds;     // Nombre de commandes
+    char    **env;        // Environnement
+    int     exit_status;  // Dernier code de retour
+}   t_shell;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -3,18 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cedmarti <cedmarti@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ebigotte <ebigotte@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 14:47:56 by ebigotte          #+#    #+#             */
-/*   Updated: 2025/02/19 13:59:26 by cedmarti         ###   ########.fr       */
+/*   Updated: 2025/02/19 15:39:54 by ebigotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft.h"
 
-int	main(void)
+void	init_shell(t_shell *shell, char **env)
 {
-	ft_printf("Hello World !\n");
-	// coucou
+	shell->input = NULL;
+    shell->tokens = NULL;
+    shell->cmds = NULL;
+    shell->num_cmds = 0;
+    shell->env = env;  // Copie de l'environnement
+    shell->exit_status = 0;
+}
+
+int	main(int ac, char **av, char **env)
+{
+	t_shell	shell;
+
+	(void)av;
+	if (ac != 1)
+		return (0);
+	init_shell(&shell, env);
+	while (1)
+	{
+		shell.input = readline("$> ");
+		if (!shell.input || ft_strncmp(shell.input, "exit", 4) == 0)
+			break;
+		add_history(shell.input);
+		printf("input: %s\n", shell.input);
+		free(shell.input);
+	}
+	clear_history();
 	return (0);
 }
