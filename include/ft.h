ac02a362eaf82108e120a348355b29ffd3a7fabe
Wawaann <bigotte.ewan@gmail.com>
2025-02-21 11:50:44 +0100

    new struct

--- include/ft.h
+++ include/ft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft.h                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cedmarti <cedmarti@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ebigotte <ebigotte@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 14:46:06 by ebigotte          #+#    #+#             */
-/*   Updated: 2025/02/19 15:48:25 by cedmarti         ###   ########.fr       */
+/*   Updated: 2025/02/21 11:50:29 by ebigotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,27 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+typedef struct s_redirection
+{
+    char            *file;      // Fichier d'entrée
+    int             type;       // Type de redirection
+}           t_redirection;
+
+typedef struct s_command
+{
+    char            **args;     // Arguments de la commande
+    t_redirection   *in;        // Redirections infile
+    t_redirection   *out;       // Redirections outfile
+}           t_command;
+
 typedef struct s_shell
 {
-	char	*input;       // Ligne de commande brute (récupérée avec readline)
-	char	**tokens;     // Commande découpée en tokens
-	char	***cmds;      // Tableau des commandes séparées par `|`
-	int		num_cmds;     // Nombre de commandes
-	char	**env;        // Environnement
-	int		exit_status;  // Dernier code de retour
-	int		*operator;		//[< , << , > , >>]
-	char	*infile;		// nom fu fichier
-	char	*outfile;		// nom du fichier
-}			t_shell;
+    char        *input;         // Ligne de commande brute (récupérée avec readline)
+    char        **tokens;       // Commande découpée en tokens
+    char        **env;          // Environnement
+    int         num_cmds;       // Nombre de commandes
+    int         exit_status;    // Dernier code de retour
+    t_command   *cmds;          // Tableau de commandes
+}           t_shell;
 
 #endif
